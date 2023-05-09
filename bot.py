4bf01d094296df5b64007e8c5cd0146715eca59f
Metoo650 <130440255+Metoo650@users.noreply.github.com>
2023-05-09 06:57:46 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,7 +1,7 @@
 import telebot
 from telebot.types import *
 
-bot = telebot.TeleBot("5769907387:AAFy8gbt2j3VQ3poDRWZMhS46Gn21X2FW7o")
+bot = telebot.TeleBot("5769907387:AAF1QPdt8rgmFOLpM-PZaeooap_Vr27dJGI")
 
 @bot.message_handler(commands =["start"])
 def start_message(message):
